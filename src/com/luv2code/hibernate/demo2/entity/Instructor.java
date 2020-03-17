b01b9ed3b8029c8ce892eba18b35524e9014eec6
marco <amantil@protonmail.com>
2020-03-17 16:54:02 +0100

    @OneToMany relation. Bidirectional. not cascade on delete.

--- src/com/luv2code/hibernate/demo2/entity/Instructor.java
+++ src/com/luv2code/hibernate/demo2/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.luv2code.hibernate.demo2.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,9 +10,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+/*
+ * Relation one to one Instructor -> Instructor detail. Bidirectional. Operation cascade ALL.
+ */
 @Entity
 @Table(name="instructor")
 public class Instructor {
@@ -45,6 +53,11 @@ public class Instructor {
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
+	
+	@OneToMany(mappedBy="instructor",  
+				cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+						CascadeType.DETACH, CascadeType.REFRESH}) ////bidirectional with courses. Do not apply cascade on delete
+	private List<Course> courses;
 	
 	public Instructor() {
 		
@@ -94,6 +107,27 @@ public class Instructor {
 
 	public void setInstructorDetail(InstructorDetail instructorDetail) {
 		this.instructorDetail = instructorDetail;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+	// add convenience methods for bi-directional relationship
+	
+	public void add(Course tempCourse) {
+		
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
 	}
 
 	@Override
